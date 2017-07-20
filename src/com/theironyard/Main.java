733261336a2310350443ac48864e9f6e7ecbed45
Joe <joe-sawyer32@users.noreply.github.com>
2017-07-20 13:39:33 -0500

    Starter files added

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,7 +1,37 @@
 package com.theironyard;
 
-public class Main {
+/**
+ * Created by Joe on 7/20/17.
+ */
 
+/**
+ * Test Driven Development (TDD)
+ * Write your tests first and watch them fail.
+ * Then write the algorithm and watch the tests pass.
+ */
+public class Main {
     public static void main(String[] args) {
+        ReversePolishCalc rpc = new ReversePolishCalc();
+
+        String rpnExpression = "2.5,4.8,+";
+        double expectedResult = 7.3;
+        double actualResult = rpc.calculate(rpnExpression);
+        checkResult(rpnExpression, expectedResult, actualResult);
+
+        // Write tests for the other operators (-, *, /)
+
+        // Uncomment this test which has many operators
+//        rpnExpression = "5,1,2,+,4,*,+,3,-";
+//        expectedResult = 14.0;
+//        actualResult = rpn.calculate(rpnExpression);
+//        checkResult(rpnExpression, expectedResult, actualResult);
+    }
+
+    private static void checkResult(String expression, double expected, double actual) {
+        if (expected == actual) {
+            System.out.println("SUCCESS: " + expression + " is " + expected);
+        } else {
+            System.out.println("ERROR: " + expression + " expected " + expected + " actual " + actual);
+        }
     }
 }
